8b11d9571f8521995bad90755b536875db3e5042
Bartosz Ratajczyk <b.ratajczyk124@gmail.com>
2022-07-17 15:19:50 +0200

    last change - changing filterArray function to minimaze conditions

--- src/utility/filterArray.js
+++ src/utility/filterArray.js
@@ -1,51 +1,34 @@
 import data from "/src/store/dummy.json" assert { type: "json" };
 
-export const filterArray = (filterOption, input, array) => {
+export const filterArray = (filterOption, input, array, data) => {
   const endInput = input.toLowerCase().trim();
 
   if (endInput === "") {
     return data;
   }
-
-  switch (filterOption) {
-    case "name":
-      return filterArrayByName(endInput, array);
-    case "occupation":
-      return filterArrayByOccupation(endInput, array);
-    case "nationality":
-      return filterArrayByNationallity(endInput, array);
+  if (filterOption === "occupation") {
+    return filterArrayByOccupation(endInput, array);
   }
-};
 
-const filterArrayByName = (input, array) => {
   return array.filter((elem) => {
-    const endName = elem.name.toLowerCase();
-    const endSurname = elem.surname.toLowerCase();
-
-    if (endName.search(input) >= 0) {
-      return true;
+    const elemToFilter = elem[filterOption].toLowerCase();
+    if (filterOption === "name") {
+      const surname = elem[filterOption].toLowerCase();
+      if (surname.search(endInput) >= 0) {
+        return true;
+      }
     }
-    if (endSurname.search(input) >= 0) {
+    if (elemToFilter.search(endInput) >= 0) {
       return true;
     }
   });
 };
 
-const filterArrayByOccupation = (input, array, property) => {
+const filterArrayByOccupation = (input, array) => {
   return array.filter((elem) => {
     const endOccupation = elem.occupation.join(", ").toLowerCase();
 
     if (endOccupation.search(input) >= 0) {
-      return true;
-    }
-  });
-};
-
-const filterArrayByNationallity = (input, array) => {
-  return array.filter((elem) => {
-    const endNationality = elem.nationality.toLowerCase();
-
-    if (endNationality.search(input) >= 0) {
       return true;
     }
   });
