a4077ea564607db0776dabd4127cef4c6c6e5325
Bartosz Ratajczyk <Bartosz.Ratajczyk@itechart-group.com>
2022-07-21 11:46:50 +0200

    basic webpack config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import App from "./components/App.js";
-
+import "./styles/styles.css";
 const app = new App();
 app.live();
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    build: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+        test: /\.(png|svg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Search-Internship itechart01",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+};
