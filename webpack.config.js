50efb72cea2d91e17904279caf008945b38f938c
Bartosz Ratajczyk <Bartosz.Ratajczyk@itechart-group.com>
2022-07-21 11:52:24 +0200

    changing output folder for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,14 @@ module.exports = {
     build: path.resolve(__dirname, "src/index.js"),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "[name].[contenthash].js",
     clean: true,
     assetModuleFilename: "[name][ext]",
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "dist"),
+      directory: path.resolve(__dirname, "docs"),
     },
     port: 3000,
     open: true,
